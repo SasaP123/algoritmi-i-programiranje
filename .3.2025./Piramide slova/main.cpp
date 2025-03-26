114ffca10494bdd5007531514158fce4bbbcdd2a
SasaP123 <pilajsasa5@gmail.com>
2025-03-26 16:35:18 +0100

    Create main.cpp
    Vjezba 26.3.2025.

---
+++ .3.2025./Piramide slova/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+//--------------------------------
+    /*for(int i=1;i<=10;i++)
+        {
+            for(int j=1;j<=10;j++)
+                {
+                    cout<<j*i<<"\t";
+                }
+            cout<<endl;
+        }*/
+
+//--------------------------------
+
+    /*for(int i=1;i<=9;i++)
+        {
+            for(int razmak=1;razmak<=9-i;razmak++)
+                cout<<" ";
+            for(int j=1;j<=i;j++)
+                cout<<i;
+            cout<<endl;
+        }
+    */
+//--------------------------------
+
+    /*for(int i=1;i<=9;i++)
+        {
+            for(int razmak=1;razmak<=9-i;razmak++)
+                cout<<" ";
+            for(int j=1;j<=i;j++)
+                cout<<i<<" ";
+            cout<<endl;
+        }*/
+}
+
+
