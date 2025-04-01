45e2bf69bdef14443d3799ced1aba14e987af63d
SasaP123 <pilajsasa5@gmail.com>
2025-04-01 17:54:08 +0200

    Create main.cpp
    c++

---
+++ 04.2025./zbroj znamenki više brojeva/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int broj,zbroj=0;
+
+    do
+        {
+            cout<<"Unesi broj: ";
+                cin>>broj;
+
+            if(broj==0)
+                break;
+
+            if(broj<0)
+                broj*=1;
+
+            while(broj>0)
+                {
+                    zbroj+=broj%10;
+                    broj/=10;
+                }
+        }while(1);
+
+    cout<<"Zbroj je "<<zbroj;
+}
+
