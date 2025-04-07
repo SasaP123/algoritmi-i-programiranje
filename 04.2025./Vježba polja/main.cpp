ef45a1f5a8f041869b8ebd3851be45613eb5b641
SasaP123 <pilajsasa5@gmail.com>
2025-04-07 22:18:20 +0200

    Create main.cpp
    Vjezba i testiranje funkcioniranja polja u c++.

---
+++ 04.2025./Vježba polja/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*int polje[5];
+
+    for(int i=0;i<5;i++)
+        {
+            cout<<"Unesi vrijednost: ";
+            cin>>polje[i];
+        }
+
+    for(int i=4;i>=0;i--)
+        {
+            cout<<polje[i];
+        }*/
+
+//-------------------------------------------------------------
+    /*int n;
+
+    cout<<"Unesi n: ";
+        cin>>n; cout<<endl;
+    int polje[n];
+
+    for(int i=0;i<n;i++)
+        {
+            cout<<"Unesi vrijednost: ";
+            cin>>polje[i];
+        }
+
+    for(int i=n-1;i>=0;i--)
+        {
+            cout<<polje[i];
+        }*/
+}
