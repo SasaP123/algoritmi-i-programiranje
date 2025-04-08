c7bd56396bdcdd7aaa2dcd183b5513dc243e5224
SasaP123 <pilajsasa5@gmail.com>
2025-04-08 17:59:22 +0200

    Create main.cpp
    C++ program koji pretvara binarne brojeve u dekadske.

---
+++ 04.2025./Binarni u dekadski/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int broj;
+    int p=0;
+    int znamenka=0;
+    int zbroj=0;
+
+    cout<<"Unesi binarni broj: ";
+        cin>>broj;  cout<<endl;
+
+    int B=broj;
+
+    while(broj>0)
+        {
+            if(broj%10==1) //-ako je zadnja znamenka 1,neka se izdvaja
+                {
+                    znamenka=1;
+                    for(int i=0;i<p;i++) //-neka vrti petlju toliko puta kolika je potencija
+                        {
+                            znamenka*=2; //1*2*2*2 -> p=3
+                        }
+                }
+            broj/=10;
+            p++;
+            zbroj+=znamenka;
+            znamenka=0;
+        }
+
+    printf("Binarni broj %i je broj %i u dekadskom zapisu.",B,zbroj);
+
+
+}
